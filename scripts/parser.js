45ded827faf0892b89fb49664084f81e09cef4fe
smolga2110 <73380083+smolga2110@users.noreply.github.com>
2025-11-26 11:11:11 +0300

    Rewrite parser logic for future json

--- scripts/parser.js
+++ scripts/parser.js
@@ -7,7 +7,15 @@ async function fetchWithFallback(container, url){
         const parser = new DOMParser()
 
         const doc = parser.parseFromString(res, "text/html")
-        console.log(doc.getElementsByClassName("simple-little-table financials")[0])
+        const table = doc.getElementsByClassName("simple-little-table financials")[0]
+        const rows = table.rows;
+        console.log(rows[2].cells[1].innerText)
+        for (let i = 0; i < rows.length; i++){
+            for (let j = 3; j < rows[i].cells.length; j++)
+                if (j != 8){
+                    console.log(rows[2].cells[j - 1].innerText + rows[i].cells[0].innerText + rows[i].cells[j].innerText)
+                }
+        }
         container.innerHTML = doc.getElementsByClassName("simple-little-table financials")[0].outerHTML
     }
     catch (err){
